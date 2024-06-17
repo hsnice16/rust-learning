a1f0baad7e8145e0425730c74b66c208d2bab3e8
Himanshu Singh <hsnice16@gmail.com>
2024-06-17 09:55:11 +0530

    Result enum and panic! macro

---
+++ handle_error/src/main.rs
@@ -0,0 +1,72 @@
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
+
+fn main() {
+    // panic!("crash and burn");
+
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    // #1
+    // let greeting_file_result = File::open("hello.txt");
+
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {e:?}"),
+    //         },
+    //         _ => {
+    //             panic!("Problem open the file: {error:?}")
+    //         }
+    //     },
+    // };
+
+    // Alternative way for #1
+    // let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+    //     if error.kind() == ErrorKind::NotFound {
+    //         File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("Problem creating the file: {error:?}");
+    //         })
+    //     } else {
+    //         panic!("Problem opening the file: {error:?}");
+    //     }
+    // });
+
+    // let greeting_file = File::open("hello.txt").unwrap();
+    // let greeting_file =
+    //     File::open("hello.txt").expect("hello.txt should be included in this project");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_short() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shorter() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shortest() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
