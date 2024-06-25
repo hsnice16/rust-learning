f268aed54b40082c0cd8212e647e038debb8bb04
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-06-25 11:06:36 +0530

    generic enum and struct

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+#[derive(Debug)]
+struct Coordinate<T, U> {
+    latitude: T,
+    longitude: U,
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest::<i32>(&number_list);
@@ -10,6 +34,15 @@ fn main() {
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest::<char>(&char_list);
     println!("The largest char is {result}");
+
+    let point = Point { x: 1, y: 1 };
+    println!("Point: {point:?}");
+
+    let coordinate = Coordinate {
+        latitude: 178,
+        longitude: 23.3423444,
+    };
+    println!("Coordinate: {coordinate:?}");
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
