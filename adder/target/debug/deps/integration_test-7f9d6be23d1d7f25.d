b8b8053414061ecba8592b5f503e460606096d2d
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-06-28 01:11:52 +0530

    tests: remove target

--- adder/target/debug/deps/integration_test-7f9d6be23d1d7f25.d
+++
@@ -1,6 +0,0 @@
-/Users/himanshusingh/Documents/personal/rust-learning/adder/target/debug/deps/integration_test-7f9d6be23d1d7f25: tests/integration_test.rs tests/common/mod.rs
-
-/Users/himanshusingh/Documents/personal/rust-learning/adder/target/debug/deps/integration_test-7f9d6be23d1d7f25.d: tests/integration_test.rs tests/common/mod.rs
-
-tests/integration_test.rs:
-tests/common/mod.rs:
--- adder/target/debug/deps/integration_test-efa41098f4916dc0.d
+++
@@ -1,6 +0,0 @@
-/Users/himanshusingh/Documents/personal/rust-learning/adder/target/debug/deps/integration_test-efa41098f4916dc0.rmeta: tests/integration_test.rs tests/common/mod.rs
-
-/Users/himanshusingh/Documents/personal/rust-learning/adder/target/debug/deps/integration_test-efa41098f4916dc0.d: tests/integration_test.rs tests/common/mod.rs
-
-tests/integration_test.rs:
-tests/common/mod.rs:
