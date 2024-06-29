d5df97afe33c76de6f13eae3e126630eae854782
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-06-29 14:18:17 +0530

    library for logic

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,63 +1,17 @@
+use minigrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // let config = parse_config(&args);
-    // let config = Config::new(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    // fn new(args: &[String]) -> Self {
-    //     if args.len() < 3 {
-    //         panic!("not enough arguments");
-    //     }
-
-    //     let query = args[1].clone();
-    //     let file_path = args[2].clone();
-
-    //     Self { query, file_path }
-    // }
-
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Self { query, file_path })
-    }
-}
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let file_path = args[2].clone();
-
-//     Config { query, file_path }
-// }
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
 }
