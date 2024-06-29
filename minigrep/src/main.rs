3c2be941e1260b9918deec3c5e91d0da7068b606
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-06-29 14:12:50 +0530

    refactor code

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,63 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    // let config = parse_config(&args);
+    // let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Search for: {query}");
-    println!("Search in file: {file_path}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    // fn new(args: &[String]) -> Self {
+    //     if args.len() < 3 {
+    //         panic!("not enough arguments");
+    //     }
+
+    //     let query = args[1].clone();
+    //     let file_path = args[2].clone();
+
+    //     Self { query, file_path }
+    // }
+
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Self { query, file_path })
+    }
+}
+
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
+
+//     Config { query, file_path }
+// }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
     println!("With text:\n{contents}");
+    Ok(())
 }
