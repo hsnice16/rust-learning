71236f9881d1b53de3fe15b802da8853802da13b
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-06-29 19:12:38 +0530

    search with user input

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,6 +22,10 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
