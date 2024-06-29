11dee938b8609a4ea28b445d1b2d156c1d387bf0
Himanshu Singh <hsnice16@gmail.com>
2024-06-29 13:25:40 +0530

    read file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +9,7 @@ fn main() {
 
     println!("Search for: {query}");
     println!("Search in file: {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
 }
