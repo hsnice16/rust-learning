061a2b58a1aa6c289066ea6e359fd6d17cda5f15
Himanshu Singh <hsnice16@gmail.com>
2024-06-29 20:28:53 +0530

    send stdout to a file and stderr to screen

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
+
+// cargo run > output.txt // to print standard output messages to a file
