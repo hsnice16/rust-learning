7b1efe5c8e6ef01a7f72424f90140b1a4c1f066b
Himanshu Singh <hsnice16@gmail.com>
2024-07-13 20:50:29 +0530

    Multiple values -- Receiver waiting

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -27,10 +27,23 @@ fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {received}");
+    // let received = rx.recv().unwrap();
+    // println!("Got: {received}");
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
