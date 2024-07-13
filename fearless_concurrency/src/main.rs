1d5ce233400c2f8d9c95464dc635a6cdb9a956d2
Himanshu Singh <hsnice16@gmail.com>
2024-07-13 20:28:34 +0530

    Move ownership of values in Thread

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -9,10 +9,17 @@ fn main() {
         }
     });
 
-    handle.join().unwrap();
-
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    let v = vec![1, 2, 3];
+
+    let handle_2 = thread::spawn(move || {
+        println!("Here's a vector: {v:#?}");
+    });
+
+    handle.join().unwrap();
+    handle_2.join().unwrap();
 }
