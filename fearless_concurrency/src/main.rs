5e5c9fd1adfbd39078f148b3eb7c46031da300cb
Himanshu Singh <hsnice16@gmail.com>
2024-07-13 20:09:39 +0530

    Wait for Spawned Thread code then run Main thread

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -9,10 +9,10 @@ fn main() {
         }
     });
 
+    handle.join().unwrap();
+
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
-
-    handle.join().unwrap();
 }
