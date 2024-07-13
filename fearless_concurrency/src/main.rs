855c52db894ed84f8f3a46769282d97252a528fb
Himanshu Singh <hsnice16@gmail.com>
2024-07-13 20:46:41 +0530

    Channel: Message passing to transfer data between threads

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,25 +1,36 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {i} from the spawned thread!");
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+
+    // for i in 1..5 {
+    //     println!("hi number {i} from the main thread!");
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    // let v = vec![1, 2, 3];
+
+    // let handle_2 = thread::spawn(move || {
+    //     println!("Here's a vector: {v:#?}");
+    // });
+
+    // handle.join().unwrap();
+    // handle_2.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
-    }
-
-    let v = vec![1, 2, 3];
-
-    let handle_2 = thread::spawn(move || {
-        println!("Here's a vector: {v:#?}");
-    });
-
-    handle.join().unwrap();
-    handle_2.join().unwrap();
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
 }
