ff4300ceeabd8f31bec17c9dae00fba0913415b9
Himanshu Singh <hsnice16@gmail.com>
2024-07-15 22:50:44 +0530

    Trait Objects -- Kind of Inheritance

---
+++ gui/src/main.rs
@@ -0,0 +1,39 @@
+use gui::{Button, Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!(
+            "SelectBox drawn of height {} - width {} - options {:?}!",
+            self.height, self.width, self.options
+        );
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
