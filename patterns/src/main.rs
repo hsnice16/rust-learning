61f8efd14ef62fff85e2891bdc99a2a7f18b9781
Himanshu Singh <hsnice16@gmail.com>
2024-07-18 09:56:36 +0530

    Several ways to use Patterns

---
+++ patterns/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    // IF LET pattern --- Compiler does not check for exhaustiveness
+
+    println!("------------------ IF LET -------------------");
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tues is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    // WHILE LET pattern
+
+    println!("------------------ WHILE LET -------------------");
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{top}");
+    }
+
+    // FOR pattern
+
+    println!("------------------ FOR -------------------");
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{value} is at index {index}");
+    }
+
+    // FUNCTION PARAMETERS pattern
+
+    println!("------------------ FUNCTION PARAMETERS -------------------");
+
+    fn print_coordinates(&(x, y): &(i32, i32)) {
+        println!("Current location: ({x}, {y})");
+    }
+
+    let point = (3, 5);
+    print_coordinates(&point);
+}
