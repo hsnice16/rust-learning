414bd58e92252c318f35b44e9009d8b3eb911e62
Himanshu Singh <hsnice16@gmail.com>
2024-07-19 23:20:35 +0530

    Fully Qualified Syntax for Disambiguation: Calling Methods with the Same Name

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -28,11 +28,67 @@ impl Add<Meters> for Millimeters {
     }
 }
 
+// Fully Qualified Syntax for Disambiguation: Calling Methods with the Same Name
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!")
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*")
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3 }
-    )
+    );
+
+    let person = Human;
+    person.fly();
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
 }
