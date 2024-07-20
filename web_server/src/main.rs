df99c57b66d6f1a459ba498c02f6af1d0b407e99
Himanshu Singh <hsnice16@gmail.com>
2024-07-20 20:45:22 +0530

    Spawning a Thread for Each Request

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -12,17 +12,14 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
