821157c4724cc582778951d49834472f6b5a70ca
Himanshu Singh <hsnice16@gmail.com>
2024-07-20 20:48:44 +0530

    Creating a Finite Number of Threads

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -8,11 +8,12 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
