88ccd4d4e95ff060d884d10cb085519441403d30
Himanshu Singh <hsnice16@gmail.com>
2025-02-19 01:55:56 +0530

    learn: serialize and deserialize

--- ardan-labs/authentication/src/lib.rs
+++ ardan-labs/authentication/src/lib.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, path::Path};
 
 pub fn greet_user(name: &str) -> String {
     format!("Hello {name}")
@@ -10,13 +11,13 @@ pub enum LoginAction {
     Denied,
 }
 
-#[derive(PartialEq, Debug, Clone)]
+#[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
 pub enum LoginRole {
     Admin,
     User,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     pub username: String,
     pub password: String,
@@ -41,7 +42,7 @@ impl User {
 //     ]
 // }
 
-fn get_users() -> HashMap<String, User> {
+fn get_default_users() -> HashMap<String, User> {
     let mut users = HashMap::new();
 
     users.insert(
@@ -54,6 +55,22 @@ fn get_users() -> HashMap<String, User> {
     );
 
     users
+}
+
+fn get_users() -> HashMap<String, User> {
+    let users_path = Path::new("users.json");
+    if users_path.exists() {
+        // Load the file!
+        let users_json = std::fs::read_to_string(users_path).unwrap();
+        let users: HashMap<String, User> = serde_json::from_str(&users_json).unwrap();
+        users
+    } else {
+        // Create a file and return it
+        let users = get_default_users();
+        let users_json = serde_json::to_string(&users).unwrap();
+        std::fs::write(users_path, users_json).unwrap();
+        users
+    }
 }
 
 // fn get_admin_users() {
