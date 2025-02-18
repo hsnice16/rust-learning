849f766e13a9f33308e6469154e359e8e8027f27
Himanshu Singh <hsnice16@gmail.com>
2025-02-18 16:30:22 +0530

    login feat

--- ardan-labs/authentication/src/lib.rs
+++ ardan-labs/authentication/src/lib.rs
@@ -2,6 +2,18 @@ pub fn greet_user(name: &str) -> String {
     format!("Hello {name}")
 }
 
+pub fn login(username: &str, password: &str) -> bool {
+    username.to_lowercase() == "admin" && password == "password"
+}
+
+pub fn read_line() -> String {
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Stdin not working");
+    input.trim().to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +21,13 @@ mod tests {
     #[test]
     fn test_greet_user() {
         assert_eq!(greet_user("John"), "Hello John");
+    }
+
+    #[test]
+    fn test_login() {
+        assert!(login("ADMIN", "password"));
+        assert!(login("admin", "password"));
+        assert!(!login("admin-not", "password"));
+        assert!(!login("admin", "not-password"));
     }
 }
--- ardan-labs/login/src/main.rs
+++ ardan-labs/login/src/main.rs
@@ -1,6 +1,25 @@
-use authentication::greet_user;
+use authentication::{greet_user, login, read_line};
 
 fn main() {
-    println!("Hello, world!");
-    println!("{}", greet_user("John"));
+    let mut tries = 0;
+
+    loop {
+        println!("Enter your username:");
+        let username = read_line();
+        println!("Enter your password:");
+        let password = read_line();
+
+        if login(&username, &password) {
+            println!("{}", greet_user(&username));
+            break;
+        } else {
+            println!("Incorrect username or password");
+            tries += 1;
+
+            if tries >= 3 {
+                println!("Too many failed login attempts");
+                break;
+            }
+        }
+    }
 }
