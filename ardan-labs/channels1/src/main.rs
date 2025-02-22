3579d1538d6d70e8d6565ec72ed5366631858028
Himanshu Singh <hsnice16@gmail.com>
2025-02-22 14:49:39 +0530

    learn: sending data b/w threads with channels

---
+++ ardan-labs/channels1/src/main.rs
@@ -0,0 +1,31 @@
+use std::sync::mpsc;
+
+enum Command {
+    SayHello,
+    Quit,
+}
+
+fn main() {
+    // tx - transmitter, rx - receiver
+    let (tx, rx) = mpsc::channel::<Command>();
+
+    let handle = std::thread::spawn(move || {
+        while let Ok(command) = rx.recv() {
+            match command {
+                Command::SayHello => println!("Hello"),
+                Command::Quit => {
+                    println!("Quitting");
+                    break;
+                }
+            }
+        }
+    });
+
+    for _ in 0..10 {
+        tx.send(Command::SayHello).unwrap();
+    }
+
+    println!("Sending quit");
+    tx.send(Command::Quit).unwrap();
+    handle.join().unwrap();
+}
