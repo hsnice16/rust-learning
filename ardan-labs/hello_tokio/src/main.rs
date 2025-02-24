c37f01ff0c12884c847eb72e71cad4f4760b3d65
Himanshu Singh <hsnice16@gmail.com>
2025-02-24 13:05:15 +0530

    learn: working with tokio futures awaiting yielding and spawning

--- ardan-labs/hello_tokio/src/main.rs
+++ ardan-labs/hello_tokio/src/main.rs
@@ -1,20 +1,49 @@
-// use tokio::runtime;
+// // use tokio::runtime;
 
-async fn hello() {
+async fn hello() -> u32 {
     println!("Hello Tokio");
+    3
 }
 
-// fn main() {
-//     let rt = runtime::Builder::new_multi_thread()
-//         .enable_all()
-//         .worker_threads(4)
-//         .build()
-//         .unwrap();
+// // fn main() {
+// //     let rt = runtime::Builder::new_multi_thread()
+// //         .enable_all()
+// //         .worker_threads(4)
+// //         .build()
+// //         .unwrap();
 
-//     rt.block_on(hello());
+// //     rt.block_on(hello());
+// // }
+
+// async fn hello2() -> u32 {
+//     println!("Hello Tokio2");
+//     4
 // }
 
-#[tokio::main]
+async fn ticker() {
+    for i in 0..10 {
+        println!("tick {i}");
+        tokio::task::yield_now().await;
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
-    hello().await;
+    // hello().await;
+
+    // let result = tokio::join!(hello(), hello2());
+    // println!("{result:?}");
+
+    // let (one, two) = result;
+    // println!("one: {one}, two: {two}");
+
+    // tokio::spawn(ticker());
+    // // hello().await;
+
+    let _ = tokio::join!(
+        tokio::spawn(hello()),
+        tokio::spawn(ticker()),
+        tokio::spawn(ticker())
+    );
+    println!("Finished");
 }
