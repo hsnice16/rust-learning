ec04ca4dbb14e7ddff4a17b8cf89d855afa2d093
Himanshu Singh <hsnice16@gmail.com>
2025-02-25 18:31:07 +0530

    run fixes

--- ardan-labs/restclient/src/main.rs
+++ ardan-labs/restclient/src/main.rs
@@ -1,22 +1,24 @@
-#[derive(Deserialize, Debug)]
-struct CurrentWeather {
-    temperature: f64,
-    windspeed: f64,
-}
+// #[derive(Deserialize, Debug)]
+// struct CurrentWeather {
+//     temperature: f64,
+//     windspeed: f64,
+// }
 
-#[derive(Deserialize, Debug)]
-struct Weather {
-    latitude: f64,
-    longitude: f64,
-    current_weather: CurrentWeather,
-}
+// #[derive(Deserialize, Debug)]
+// struct Weather {
+//     latitude: f64,
+//     longitude: f64,
+//     current_weather: CurrentWeather,
+// }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     const URL: &str = "https://api.open-meteo.com/v1/forecast";
     let response = reqwest::get(URL).await?;
-    let weather: serde_json::Value = response.json().await?;
+    // let weather: serde_json::Value = response.text().await?;
+    let weather = response.text().await?;
+
     println!("{weather:#?}");
-    
+
     Ok(())
 }
