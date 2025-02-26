15c44e8904d8bad3623e7f05c8447cf099204490
Himanshu Singh <hsnice16@gmail.com>
2025-02-26 18:52:38 +0530

    learn: The unsafe Keyword

---
+++ ardan-labs/memory/src/main.rs
@@ -0,0 +1,13 @@
+/// # Safety
+///
+/// This function is unsafe because...
+unsafe fn my_fn() {}
+
+fn main() {
+    let my_vec = vec![1, 2, 3, 4, 5];
+
+    // my_vec[20];
+
+    // if let Some(value) = my_vec.get(2) {
+    // unsafe { if let Some(value) = my_vec.get_unchecked(2) {} }
+}
