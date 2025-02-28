26a0a55b7ea82343248d9b8598b8c1043934010a
Himanshu Singh <hsnice16@gmail.com>
2025-02-28 19:16:27 +0530

    learn: packing reordering mangling

---
+++ ardan-labs/packing/src/main.rs
@@ -0,0 +1,27 @@
+struct OneByte {
+    a: u8,
+}
+
+struct TwoBytes {
+    a: u16,
+}
+
+// #[repr(packed)]
+
+#[no_mangle]
+#[repr(C)]
+struct ThreeBytes {
+    a: u16,
+    b: u8,
+}
+
+struct FourBytes {
+    a: u32,
+}
+
+fn main() {
+    println!("{}", std::mem::size_of::<OneByte>());
+    println!("{}", std::mem::size_of::<TwoBytes>());
+    println!("{}", std::mem::size_of::<ThreeBytes>());
+    println!("{}", std::mem::size_of::<FourBytes>());
+}
