c05964db1f77dabe6c6e982590e9912ff35c4ba1
Himanshu Singh <hsnice16@gmail.com>
2025-02-28 18:45:50 +0530

    learn: Iterators

---
+++ ardan-labs/iterators/src/main.rs
@@ -0,0 +1,94 @@
+use std::collections::HashMap;
+
+struct Counter {
+    count: u32,
+    max: u32,
+}
+
+impl Counter {
+    fn new(max: u32) -> Counter {
+        Counter { count: 0, max }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < self.max {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+impl ExactSizeIterator for Counter {
+    fn len(&self) -> usize {
+        self.max as usize
+    }
+}
+
+impl<K, V> HashMapBucket<K, V> {
+    fn iter(&self) -> HashMapBucketIter<K, V> {
+        let mut key_iter = self.map.iter();
+        let current_map_entry = key_iter.next();
+        HashMapBucketIter {
+            key_iter,
+            current_map_entry,
+            current_vec_index: 0,
+        }
+    }
+}
+
+struct HashMapBucketIter<'a, K, V> {
+    key_iter: std::collections::hash_map::Iter<'a, K, Vec<V>>,
+    current_map_entry: Option<(&'a K, &'a Vec<V>)>,
+    current_vec_index: usize,
+}
+
+// Specify 'a - the lifetime, and K, V on both sides.
+// If you wanted to change how the iterator acts for a give type of key or
+// value you could change the left-hand side.
+impl<'a, K, V> Iterator for HashMapBucketIter<'a, K, V> {
+    // Define `Item` - a type used inside the trait - to be a reference to a key and value.
+    // This specifies the type that the iterator will return.
+    type Item = (&'a K, &'a V);
+
+    // You use Item to specify the type returned by `Next`. It's always an option of the type.
+    fn next(&mut self) -> Option<Self::Item> {
+        // If there is a current map entry, and a current vec index
+        if let Some((key, values)) = self.current_map_entry {
+            // If the index is less than the lenght of the vector
+            if self.current_vec_index < values.len() {
+                // Get the value at the current index
+                let value = &values[self.current_vec_index];
+                // Increment the index
+                self.current_vec_index += 1;
+                // Return the key and value
+                return Some((key, value));
+            } else {
+                // We're past the end of the vector, next key
+                self.current_map_entry = self.key_iter.next();
+                self.current_vec_index = 0;
+
+                if let Some((key, values)) = self.current_map_entry {
+                    // If the index is less than the lenght of the vector
+                    if self.current_vec_index < values.len() {
+                        // Get the value at the current index
+                        let value = &values[self.current_vec_index];
+                        // Increment the index
+                        self.current_vec_index += 1;
+                        // Return the key and value
+                        return Some((key, value));
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let numbers: Vec<u32> = Counter::new(10).collect();
+    println!("{numbers:?}");
+}
