bfb866fa13ed0a89490c61cc66f66a19a0b4e4ec
Himanshu Singh <hsnice16@gmail.com>
2025-03-03 19:28:32 +0530

    learn: building cw contract

---
+++ empty-contract/src/contract.rs
@@ -0,0 +1,350 @@
+use crate::{
+    error::ContractError,
+    msg::{ExecuteMsg, GreetResp, InstantiateMsg, QueryMsg},
+    state::ADMINS,
+};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, to_binary};
+
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
+) -> StdResult<Response> {
+    let admins: StdResult<Vec<_>> = msg
+        .admins
+        .into_iter()
+        .map(|addr| deps.api.addr_validate(&addr))
+        .collect();
+
+    ADMINS.save(deps.storage, &admins?)?;
+
+    Ok(Response::new())
+}
+
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    use QueryMsg::*;
+
+    match msg {
+        Greet {} => to_binary(&query::greet()?),
+        AdminsList {} => to_binary(&query::admins_list(deps)?),
+    }
+}
+
+mod query {
+    use crate::msg::AdminsListResp;
+
+    use super::*;
+
+    pub fn greet() -> StdResult<GreetResp> {
+        let resp = GreetResp {
+            message: "Hello World".to_owned(),
+        };
+
+        Ok(resp)
+    }
+
+    pub fn admins_list(deps: Deps) -> StdResult<AdminsListResp> {
+        let admins = ADMINS.load(deps.storage)?;
+        let resp = AdminsListResp { admins };
+        Ok(resp)
+    }
+}
+
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    use ExecuteMsg::*;
+
+    match msg {
+        AddMembers { admins } => exec::add_members(deps, info, admins),
+        Leave {} => exec::leave(deps, info).map_err(Into::into),
+    }
+}
+
+mod exec {
+    use cosmwasm_std::Event;
+
+    use crate::error::ContractError;
+
+    use super::*;
+
+    pub fn add_members(
+        deps: DepsMut,
+        info: MessageInfo,
+        admins: Vec<String>,
+    ) -> Result<Response, ContractError> {
+        let mut curr_admins = ADMINS.load(deps.storage)?;
+        if !curr_admins.contains(&info.sender) {
+            return Err(ContractError::Unauthorized {
+                sender: info.sender,
+            });
+        }
+
+        let events = admins
+            .iter()
+            .map(|admin| Event::new("admin_added").add_attribute("addr", admin));
+        let resp = Response::new()
+            .add_events(events)
+            .add_attribute("action", "add_members")
+            .add_attribute("added_count", admins.len().to_string());
+
+        let admins: StdResult<Vec<_>> = admins
+            .into_iter()
+            .map(|addr| deps.api.addr_validate(&addr))
+            .collect();
+
+        curr_admins.append(&mut admins?);
+        ADMINS.save(deps.storage, &curr_admins)?;
+
+        Ok(resp)
+    }
+
+    pub fn leave(deps: DepsMut, info: MessageInfo) -> StdResult<Response> {
+        ADMINS.update(deps.storage, move |admins| -> StdResult<_> {
+            let admins = admins
+                .into_iter()
+                .filter(|admin| *admin != info.sender)
+                .collect();
+            Ok(admins)
+        })?;
+
+        Ok(Response::new())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // With Multitest
+
+    use cosmwasm_std::Addr;
+    use cw_multi_test::{App, ContractWrapper, Executor};
+
+    use crate::msg::AdminsListResp;
+
+    use super::*;
+
+    #[test]
+    fn add_members() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg {
+                    admins: vec!["owner".to_owned()],
+                },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp = app
+            .execute_contract(
+                Addr::unchecked("owner"),
+                addr,
+                &ExecuteMsg::AddMembers {
+                    admins: vec!["user".to_owned()],
+                },
+                &[],
+            )
+            .unwrap();
+
+        let wasm = resp.events.iter().find(|ev| ev.ty == "wasm").unwrap();
+        assert_eq!(
+            wasm.attributes
+                .iter()
+                .find(|attr| attr.key == "action")
+                .unwrap()
+                .value,
+            "add_members"
+        );
+        assert_eq!(
+            wasm.attributes
+                .iter()
+                .find(|attr| attr.key == "added_count")
+                .unwrap()
+                .value,
+            "1"
+        );
+
+        let admin_added: Vec<_> = resp
+            .events
+            .iter()
+            .filter(|ev| ev.ty == "wasm-admin_added")
+            .collect();
+        assert_eq!(admin_added.len(), 1);
+
+        assert_eq!(
+            admin_added[0]
+                .attributes
+                .iter()
+                .find(|attr| attr.key == "addr")
+                .unwrap()
+                .value,
+            "user"
+        )
+    }
+
+    #[test]
+    fn unauthorized() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg { admins: vec![] },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let err = app
+            .execute_contract(
+                Addr::unchecked("user"),
+                addr,
+                &ExecuteMsg::AddMembers {
+                    admins: vec!["user".to_owned()],
+                },
+                &[],
+            )
+            .unwrap_err();
+
+        assert_eq!(
+            ContractError::Unauthorized {
+                sender: Addr::unchecked("user")
+            },
+            err.downcast().unwrap()
+        )
+    }
+
+    #[test]
+    fn admins_query() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg { admins: vec![] },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp, AdminsListResp { admins: vec![] });
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg {
+                    admins: vec!["admin1".to_owned(), "admin2".to_owned()],
+                },
+                &[],
+                "Contract 2",
+                None,
+            )
+            .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(
+            resp,
+            AdminsListResp {
+                admins: vec![Addr::unchecked("admin1"), Addr::unchecked("admin2")],
+            }
+        )
+    }
+
+    #[test]
+    fn greet_query() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg { admins: vec![] },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: GreetResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::Greet {})
+            .unwrap();
+
+        assert_eq!(
+            resp,
+            GreetResp {
+                message: "Hello World".to_owned()
+            }
+        )
+    }
+
+    // Without Multitest
+
+    // use cosmwasm_std::{
+    //     from_binary,
+    //     testing::{mock_dependencies, mock_env, mock_info},
+    // };
+
+    // use super::*;
+
+    // #[test]
+    // fn greet_query() {
+    //     let mut deps = mock_dependencies();
+    //     let env = mock_env();
+
+    //     instantiate(
+    //         deps.as_mut(),
+    //         env.clone(),
+    //         mock_info("sender", &[]),
+    //         Empty {},
+    //     )
+    //     .unwrap();
+
+    //     // let resp = query::greet().unwrap();
+
+    //     let resp = query(deps.as_ref(), env, QueryMsg::Greet {}).unwrap();
+    //     let resp: GreetResp = from_binary(&resp).unwrap();
+
+    //     assert_eq!(
+    //         resp,
+    //         GreetResp {
+    //             message: "Hello World".to_owned()
+    //         }
+    //     )
+    // }
+}
---
+++ empty-contract/src/lib.rs
@@ -0,0 +1,33 @@
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, entry_point};
+use error::ContractError;
+use msg::{ExecuteMsg, InstantiateMsg};
+
+mod contract;
+mod error;
+mod msg;
+mod state;
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> StdResult<Response> {
+    contract::instantiate(deps, env, info, msg)
+}
+
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
+    contract::query(deps, env, msg)
+}
+
+#[entry_point]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    contract::execute(deps, env, info, msg)
+}
